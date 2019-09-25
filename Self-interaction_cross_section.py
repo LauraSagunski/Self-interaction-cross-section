42ad55219a372eb43da65fc8a3657f6502e87db9
LauraSagunski <45136736+LauraSagunski@users.noreply.github.com>
2019-09-25 12:08:38 +0200

    Updated code.

--- Self-interaction_cross_section.py
+++ Self-interaction_cross_section.py
@@ -3,13 +3,14 @@
 
 # # Packages
 
-# In[197]:
+# In[301]:
 
 
 import numpy as np
 import pandas as pd #for loading csv Excel files
 import itertools #to merge lists
 from scipy.interpolate import interp1d
+from scipy.optimize import leastsq, curve_fit #for fitting
 import matplotlib.pyplot as pl #for plots
 from matplotlib import rc, rcParams
 
@@ -46,12 +47,19 @@ colors=[lightblue,blue,darkblue,purple,magenta,red,orange,yellow,green]
 
 # # Constants
 
-# In[177]:
+# In[288]:
 
 
 c=2.99792458*10.**8.*10.**(-3.) #km/s (not m/s)
-GeV_to_cmminus1=5.06*10.**13.
+GeV_to_cmminus1=5.068*10.**13.
 GeV_to_g=1.78*10.**(-24.)
+fm_to_cm=10.**(-15.)*10.**2.
+
+
+# In[274]:
+
+
+fm_to_cm
 
 
 # # Data dark photon model
@@ -163,6 +171,220 @@ pl.xlabel(r'$v\;{\rm (km/s)}$',fontsize=fontsize)
 pl.ylabel(r'$\sigma_T \, m_{\chi}^{2}\;{\rm (cm^{2}\,g^{-1}\,GeV^{3})}$',fontsize=fontsize) 
 pl.legend(title=r'$b$',loc='upper right',edgecolor='white',fontsize=legendfontsize)
 pl.show() #Ok. In accordance with Fig. 3 in Sean's paper 1302.3898.
+
+
+# # Parameterization of sigmaT(v) from Chu et al., 1908.06067
+
+# In[290]:
+
+
+def sigmaTChu(mX,v,aa,re):
+    A=mX*(aa*fm_to_cm*GeV_to_cmminus1)*(v/c) #aa in GeV**(-1.)
+    B=re/aa #re in GeV**(-1.)
+    term1=(1-(1./8.)*B*A**2.)**2.
+    term2=(1./4.)*A**2.
+    #print('[A,B,term1,term2]='+str([A,B,term1,term2]))
+    sigmaT=4.*np.pi*(aa*fm_to_cm)**2.*(term1+term2)**(-1.)
+    return sigmaT
+
+def sigmaT_over_mChu(mX,v,aa,re):
+    sigmaT_over_m=sigmaTChu(mX,v,aa,re)/(mX*GeV_to_g) #cm**2/g
+    return sigmaT_over_m
+
+def sigmaT_times_m2Chu(mX,v,aa,re):
+    sigmaT_times_m2=sigmaT_over_mChu(mX,v,aa,re)*mX**3. #cm**2/g*GeV**3.
+    return sigmaT_times_m2
+
+for mX in [200]: #GeV
+    for v in [10.]: #km/s
+        for aa in [10]: #fm
+            for re in [1.]: #fm
+                print('sigmaT [cm^2]='+str(sigmaTChu(mX,v,aa,re)))
+                print('sigmaT/m [cm^2/g]='+str(sigmaT_over_mChu(mX,v,aa,re)))
+                print('sigmaT*m [(cm^2/g) GeV^3]='+str(sigmaT_times_m2Chu(mX,v,aa,re)))
+
+
+# # Fit of Sean's cross section data with parameterization of Chu
+
+# In[382]:
+
+
+vvals=np.logspace(np.log10(1.),np.log10(10.**4.),1000)
+print(vvals)
+
+
+# In[363]:
+
+
+def sigmaTChu(mX,v,aa,ratio):
+    A=mX*(aa*fm_to_cm*GeV_to_cmminus1)*(v/c) #aa in GeV**(-1.)
+    B=ratio
+    #B=re/aa #re in GeV**(-1.)
+    term1=(1-(1./8.)*B*A**2.)**2.
+    term2=(1./4.)*A**2.
+    #print('[A,B,term1,term2]='+str([A,B,term1,term2]))
+    sigmaT=4.*np.pi*(aa*fm_to_cm)**2.*(term1+term2)**(-1.)
+    return sigmaT
+
+def sigmaT_over_mChu(mX,v,aa,ratio):
+    sigmaT_over_m=sigmaTChu(mX,v,aa,ratio)/(mX*GeV_to_g) #cm**2/g
+    return sigmaT_over_m
+
+def sigmaT_times_m2Chu(mX,v,aa,ratio):
+    sigmaT_times_m2=sigmaT_over_mChu(mX,v,aa,ratio)*mX**3. #cm**2/g*GeV**3.
+    return sigmaT_times_m2
+
+for mX in [10**2.]: #GeV
+    for v in [10.]: #km/s
+        for aa in [10.]: #fm
+            for ratio in [-1.]: 
+                print('sigmaT [cm^2]='+str(sigmaTChu(mX,v,aa,ratio)))
+                print('sigmaT/m [cm^2/g]='+str(sigmaT_over_mChu(mX,v,aa,ratio)))
+                print('sigmaT*m [(cm^2/g) GeV^3]='+str(sigmaT_times_m2Chu(mX,v,aa,ratio)))
+
+
+# In[383]:
+
+
+potential='attractive'
+alphaX=10.**(-2.)
+mX=100. #GeV
+#b=4.52
+#mphi=(alphaX*mX)/b
+mphi=1. #GeV
+print('mphi='+str(mphi))
+
+sigmaT_times_m2vals=np.array([sigmaT_times_m2(potential,alphaX,mX,mphi,v) for v in vvals])
+
+def sigmaT_times_m2Chufit(vvals,aa,ratio):
+    sigmaT_times_m2valsChu=np.array([sigmaT_times_m2Chu(mX,v,aa,ratio) for v in vvals])
+    return sigmaT_times_m2valsChu
+
+popt, pcov =curve_fit(sigmaT_times_m2Chufit,vvals,sigmaT_times_m2vals)
+print(popt, pcov)
+[aa,ratio]=popt
+re=ratio*aa
+sigmaT_times_m2valsChu=np.array([sigmaT_times_m2Chu(mX,v,aa,ratio) for v in vvals])
+chi2=sum(np.array(sigmaT_times_m2vals-sigmaT_times_m2valsChu)**2.)
+print('[aa,ratio,re,chi2]='+str([aa,ratio,re,chi2]))
+
+
+# In[385]:
+
+
+pl.plot(vvals,sigmaT_times_m2valsChu,label='fit')
+pl.plot(vvals,sigmaT_times_m2vals,label='Sean')
+pl.xscale('log')
+pl.yscale('log')
+pl.xlim([1,10.**4.])
+#pl.ylim([0.1,10.**9.])
+pl.xlabel(r'$v\;{\rm (km/s)}$',fontsize=fontsize) 
+pl.ylabel(r'$\sigma_T \, m_{\chi}^{2}\;{\rm (cm^{2}\,g^{-1}\,GeV^{3})}$',fontsize=fontsize) 
+pl.legend(title=r'$m_{\phi}=$'+str(mphi)+r'$\,{\rm GeV}$',bbox_to_anchor=[1.,1.],edgecolor='white',fontsize=legendfontsize)
+pl.show()
+
+
+# In[388]:
+
+
+pl.plot(vvals,(sigmaT_times_m2vals-sigmaT_times_m2valsChu)/sigmaT_times_m2valsChu)
+pl.xscale('log')
+#pl.yscale('log')
+pl.xlim([1,10.**4.])
+#pl.ylim([0.1,10.**9.])
+pl.xlabel(r'$v\;{\rm (km/s)}$',fontsize=fontsize) 
+#pl.ylabel(r'$\sigma_{T\,{\rm Sean}}-\sigma_{T\,{\rm Chu}} \, m_{\chi}^{2}\;{\rm (cm^{2}\,g^{-1}\,GeV^{3})}$',fontsize=fontsize) 
+pl.legend(title=r'$m_{\phi}=$'+str(mphi)+r'$\,{\rm GeV}$',bbox_to_anchor=[1.,1.],edgecolor='white',fontsize=legendfontsize)
+pl.show()
+
+
+# In[389]:
+
+
+potential='attractive'
+alphaX=10.**(-2.)
+mX=100. #GeV
+#b=4.52
+#mphi=(alphaX*mX)/b
+mphi=0.001 #GeV
+print('mphi='+str(mphi))
+
+sigmaT_times_m2vals=np.array([sigmaT_times_m2(potential,alphaX,mX,mphi,v) for v in vvals])
+
+def sigmaT_times_m2Chufit(vvals,aa,ratio):
+    sigmaT_times_m2valsChu=np.array([sigmaT_times_m2Chu(mX,v,aa,ratio) for v in vvals])
+    return sigmaT_times_m2valsChu
+
+popt, pcov =curve_fit(sigmaT_times_m2Chufit,vvals,sigmaT_times_m2vals,bounds=((0.,100.)),max_nfev=100)
+print(popt, pcov)
+[aa,ratio]=popt
+re=ratio*aa
+sigmaT_times_m2valsChu=np.array([sigmaT_times_m2Chu(mX,v,aa,ratio) for v in vvals])
+chi2=sum(np.array(sigmaT_times_m2vals-sigmaT_times_m2valsChu)**2.)
+print('[aa,ratio,re,chi2]='+str([aa,ratio,re,chi2]))
+
+
+# In[390]:
+
+
+pl.plot(vvals,sigmaT_times_m2valsChu,'.'.label='fit')
+pl.plot(vvals,sigmaT_times_m2vals,label='Sean')
+pl.xscale('log')
+pl.yscale('log')
+pl.xlim([1,10.**4.])
+#pl.ylim([0.1,10.**9.])
+pl.xlabel(r'$v\;{\rm (km/s)}$',fontsize=fontsize) 
+pl.ylabel(r'$\sigma_T \, m_{\chi}^{2}\;{\rm (cm^{2}\,g^{-1}\,GeV^{3})}$',fontsize=fontsize) 
+pl.legend(title=r'$m_{\phi}=$'+str(mphi)+r'$\,{\rm GeV}$',bbox_to_anchor=[1.,1.],edgecolor='white',fontsize=legendfontsize)
+pl.show()
+
+
+# In[376]:
+
+
+vvals=np.logspace(np.log10(10.**(-2.)),np.log10(10.**2.),1000)
+print(vvals)
+
+
+# In[377]:
+
+
+potential='attractive'
+alphaX=10.**(-2.)
+mX=100. #GeV
+#b=4.52
+#mphi=(alphaX*mX)/b
+mphi=1. #GeV
+print('mphi='+str(mphi))
+
+sigmaT_times_m2vals=np.array([sigmaT_times_m2(potential,alphaX,mX,mphi,v) for v in vvals])
+
+def sigmaT_times_m2Chufit(vvals,aa,ratio):
+    sigmaT_times_m2valsChu=np.array([sigmaT_times_m2Chu(mX,v,aa,ratio) for v in vvals])
+    return sigmaT_times_m2valsChu
+
+popt, pcov =curve_fit(sigmaT_times_m2Chufit,vvals,sigmaT_times_m2vals)
+print(popt, pcov)
+[aa,ratio]=popt
+re=ratio*aa
+sigmaT_times_m2valsChu=np.array([sigmaT_times_m2Chu(mX,v,aa,ratio) for v in vvals])
+chi2=sum(np.array(sigmaT_times_m2vals-sigmaT_times_m2valsChu)**2.)
+print('[aa,ratio,re,chi2]='+str([aa,ratio,re,chi2]))
+
+
+# In[378]:
+
+
+pl.plot(vvals,sigmaT_times_m2valsChu,label='fit')
+pl.plot(vvals,sigmaT_times_m2vals,label='Sean')
+pl.xscale('log')
+pl.yscale('log')
+pl.xlim([1,10.**4.])
+#pl.ylim([0.1,10.**9.])
+pl.xlabel(r'$v\;{\rm (km/s)}$',fontsize=fontsize) 
+pl.ylabel(r'$\sigma_T \, m_{\chi}^{2}\;{\rm (cm^{2}\,g^{-1}\,GeV^{3})}$',fontsize=fontsize) 
+pl.legend(title=r'$m_{\phi}=$'+str(mphi)+r'$\,{\rm GeV}$',bbox_to_anchor=[1.,1.],edgecolor='white',fontsize=legendfontsize)
+pl.show()
 
 
 # In[ ]:
